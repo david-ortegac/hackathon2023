d5e8e0119f00b5743b89d962dd8998b67d36def9
david-ortegac <davidortegacadena@gmail.com>
2023-10-18 19:52:48 -0500

    trabajar

--- src/app/components/tabs/trabajar/trabajar.component.ts
+++ src/app/components/tabs/trabajar/trabajar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-trabajar',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./trabajar.component.css']
 })
 export class TrabajarComponent {
+  profession: string = "";
+  selected: number = 0;
+
+  constructor()
+  {}
+
+  onSelected(value:string) {
+    if (value == "Sin estudios" || value == "Bachiller") {
+      this.selected = 1;
+    } else if (value == "Técnico") {
+      this.selected = 2;
+    } else if (value == "Tecnólogo") {
+      this.selected = 3;
+    } else if (value == "Profesional") {
+      this.selected = 4;
+    } else {
+      this.selected = 0;
+    }
+  }
+
+  buscarEmpleo() {
+    Swal.fire({
+      title: 'Quieres enviar la hoja de vida?',
+      text: "Recuerda que las vacantes se ajustan a tu lugar de residencia!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, enviar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Enviada!',
+          'Tu hoja de vida ha sido enviada, pronto nos contactaremos.',
+          'success'
+        )
+      }
+    });
+  }
+
+  capacitacionVirtual() {
+    Swal.fire({
+      title: 'Quieres recibir capacitación virtual?',
+      text: "Recuerda que las capacitaciones tienen cupos limitados!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, enviar solicitud!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Enviada!',
+          'Tu solicitud ha sido enviada, pronto nos contactaremos.',
+          'success'
+        )
+      }
+    });
+  }
+
 
 }
