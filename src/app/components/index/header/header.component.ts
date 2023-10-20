6bcad7f20954b54ca8d2729f78eb1eccfb4b29ff
david-ortegac <davidortegacadena@gmail.com>
2023-10-19 19:37:21 -0500

    ajustes finales, bug en el cerrado de navbar

--- src/app/components/index/header/header.component.ts
+++ src/app/components/index/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+  ) { }
+  
+  showMenu() {
+      this.renderer.removeClass(this.document.body, 'mobile-nav-active');
+  }
 
 }
--- src/app/components/tabs/contacto/contacto.component.ts
+++ src/app/components/tabs/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+
+  send() {
+    Swal.fire('Mensaje enviado!')
+  }
 
 }
